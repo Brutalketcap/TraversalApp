e1f12b56422601bb8250ccb77c35b7ff1b58d49d
OzanAteş <112420940+ozan.ates@users.noreply.github.com>
2025-02-18 17:29:25 +0300

    update

---
+++ BussinessLayer/ValidationRules/AboutValidator.cs
@@ -0,0 +1,26 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessLayer.ValidationRules
+{
+    public class AboutValidator : AbstractValidator<About>
+    {
+        public AboutValidator()
+        {
+            RuleFor(x=>x.Description).NotEmpty().WithMessage("Açıklama alanı boş geçilemez");
+
+            RuleFor(x => x.Image).NotEmpty().WithMessage("Lütfen gösresl seçiniz...!");
+
+            RuleFor(x=> x.Description).MinimumLength(50).WithMessage("Açıklama alanı en az 50 karakter olmalıdır");
+
+            RuleFor(x => x.Description).MaximumLength(50).WithMessage("Lütfen açıklamayı kısaltın..!");
+
+
+        }
+    }
+}
